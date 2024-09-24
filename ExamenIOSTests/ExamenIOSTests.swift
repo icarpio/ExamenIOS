dc9964b2ffbce1c3b29c923f89b5dda9b448d3b9
icarpio <icarpiodeveloper@gmail.com>
2024-09-24 09:26:27 +0200

    Initial Commit

---
+++ ExamenIOSTests/ExamenIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenIOSTests.swift
+//  ExamenIOSTests
+//
+//  Created by Mañanas on 24/9/24.
+//
+
+import Testing
+@testable import ExamenIOS
+
+struct ExamenIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
